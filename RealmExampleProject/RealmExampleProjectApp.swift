bde578308882f7ddf4fda420932eba8e3aac1fda
dalkilicyasin <Yd123456+*>
2024-06-22 08:41:01 +0300

    Initial Commit

---
+++ RealmExampleProject/RealmExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmExampleProjectApp.swift
+//  RealmExampleProject
+//
+//  Created by yasin on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct RealmExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
